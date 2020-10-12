a98a2291b7fa24958678e24f1e4bdc9b42a5046a
BudhiParkash <shivam.dev389@gmail.com>
2020-10-12 23:45:03 +0530

    field and constructor inject

--- app/src/main/java/com/example/daggerhiltbasic/MainActivity.java
+++ app/src/main/java/com/example/daggerhiltbasic/MainActivity.java
@@ -2,13 +2,59 @@ package com.example.daggerhiltbasic;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.SocketKeepalive;
 import android.os.Bundle;
 
+import javax.inject.Inject;
+
+import dagger.hilt.android.AndroidEntryPoint;
+
+@AndroidEntryPoint
 public class MainActivity extends AppCompatActivity {
+
+    // field injection
+    @Inject
+    SomeClass someClass;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        System.out.println(someClass.dothing());
+        System.out.println(someClass.dootherrhing());
+
     }
 }
+
+class SomeClass{
+    SomeOtherClass someOtherClass;
+
+    @Inject
+    public SomeClass(SomeOtherClass someOtherClass) {
+        this.someOtherClass = someOtherClass;
+    }
+
+    public String dothing(){
+        return "Heloo filed dependecy Injection";
+    }
+
+    public String dootherrhing(){
+        return  someOtherClass.dootherthing();
+    }
+
+
+}
+
+class SomeOtherClass{
+    @Inject
+    public SomeOtherClass() {
+    }
+    public String dootherthing(){
+        return "Heloo Constructor Injection";
+    }
+}
+
+
+
---
+++ app/src/main/java/com/example/daggerhiltbasic/MyApplication.java
@@ -0,0 +1,10 @@
+package com.example.daggerhiltbasic;
+
+import android.app.Application;
+
+import dagger.hilt.android.HiltAndroidApp;
+
+@HiltAndroidApp
+public class MyApplication extends Application {
+
+}
